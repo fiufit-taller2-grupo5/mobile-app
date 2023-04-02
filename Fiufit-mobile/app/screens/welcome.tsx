61a30409cab289318272d8b46ca23db2e40d4df2
Florencia <fsardella@fi.uba.ar>
2023-04-02 10:25:30 -0300

    Updated styles of welcome page

--- Fiufit-mobile/app/screens/welcome.tsx
+++ Fiufit-mobile/app/screens/welcome.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { StyleSheet } from 'react-native';
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { Center, Container, Box, Button, AspectRatio, Image, Text, NativeBaseProvider, extendTheme } from 'native-base';
 
@@ -6,73 +7,22 @@ export default function WelcomeScreen({navigation} : NativeStackScreenProps<any,
     const theme = extendTheme({
         components: {
             Button: {
-                baseStyle: {
-                    display: 'flex',
-                    flexDirection: 'row',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    padding: '11px 22px',
-                    gap: '10px',
-                    position: 'absolute',
-                    width: '141px',
-                    height: '53px',
-                    top: '750px',
+                defaultProps: {
                     background: '#FF6060',
-                    borderRadius: '50px',
                 },
-                variants: {
-                    firstButton: {
-                        left: '10%',
-                    },
-                    secondButton: {
-                        left: '55%',
-                    }
-                }
             },
             Text: {
-                baseStyle: {
-                    fontFamily: 'BebasNeue',
-                    fontStyle: 'normal',
-                    fontWeight: '400',
-                    textAlign: 'center',
-                },
                 variants: {
                     heading1: {
-                        paddingTop: '45%',
-                        left: '0%',
-                        right: '0%',
-                        top: '0%',
-                        bottom: '0%',
-                        fontSize: '100px',
-                        lineHeight: '120px',
                         color: '#FF6060'
                     },
                     heading2: {
-                        left: '14.00%',
-                        right: '60.23%',
-                        top: '-5%',
-                        bottom: '68.24%',
-                        fontSize: '77px',
-                        lineHeight: '92px',
                         color: '#616161'
                     },
                     heading3: {
-                        left: '45.0%',
-                        right: '4.82%',
-                        top: '-27%',
-                        bottom: '67.9%',
-                        fontSize: '88px',
-                        lineHeight: '106px',
                         color: '#000000'
                     },
                     paragraph: {
-                        fontFamily: 'Roboto',
-                        top: '-75%',
-                        width: '305px',
-                        height: '160px',
-                        fontSize: '20px',
-                        lineHeight: '30px',
-                        letterSpacing: '-0.01em',
                         color: '#212121',
                     }
                 }
@@ -82,23 +32,24 @@ export default function WelcomeScreen({navigation} : NativeStackScreenProps<any,
 
     return <NativeBaseProvider theme={theme}>
         <Container>
-                <Text variant="heading1"> DESAFIA </Text>
-                <Text variant="heading2" > TUS </Text>
-                <Text variant="heading3"> LIMITES </Text>
+                <Text style={[styles.text, styles.heading1]} variant="heading1"> DESAFIA </Text>
+                <Text style={[styles.text, styles.heading2]} variant="heading2" > TUS </Text>
+                <Text style={[styles.text, styles.heading3]} variant="heading3"> LIMITES </Text>
             </Container>
         <Center>
             <Container>
-                <Text variant="paragraph">
+                <Text style={[styles.text, styles.paragraph]} variant="paragraph">
                     Realizá un seguimiento de tus entrenamientos, obtén mejores resultados y sé la mejor versión de ti mismo.
                 </Text>
             </Container>
         </Center>
-        <Box top="-22%">
+        <Box top="-18%">
           <AspectRatio w="70%" ratio={16 / 9}>
             <Image source={require('../../assets/images/welcome_image.png')} alt="image" />
           </AspectRatio>
         </Box>
-        <Button 
+        <Button
+        style={[styles.button, styles.firstButton]}
         _text={{color: "#FFFFFF"}}
         variant="firstButton"
         onPress={() => navigation.navigate('LoginScreen')}
@@ -106,6 +57,7 @@ export default function WelcomeScreen({navigation} : NativeStackScreenProps<any,
             Iniciar sesión
         </Button>
         <Button
+        style={[styles.button, styles.secondButton]}
         _text={{color: "#FFFFFF"}}
         variant="secondButton"
         onPress={() => navigation.navigate('RegisterScreen')}
@@ -115,3 +67,67 @@ export default function WelcomeScreen({navigation} : NativeStackScreenProps<any,
 
     </NativeBaseProvider>;
 }
+
+const styles = StyleSheet.create({
+    button: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingLeft: '10%',
+        paddingRight: '10%',
+        gap: 10,
+        position: 'absolute',
+        width: '35%',
+        height: '5%',
+        top: '88%',
+        left: '20%',
+        borderRadius: 50,
+    },
+    firstButton: {
+        left: '10%',
+    },
+    secondButton: {
+        left: '55%',
+    },
+    text: {
+        fontFamily: 'BebasNeue',
+        fontStyle: 'normal',
+        fontWeight: '400',
+        textAlign: 'center',
+    },
+    heading1: {
+        paddingTop: '45%',
+        left: '0%',
+        right: '0%',
+        top: '0%',
+        bottom: '0%',
+        fontSize: 100,
+        lineHeight: 120,
+    },
+    heading2: {
+        left: '14.00%',
+        right: '60.23%',
+        top: '-5%',
+        bottom: '68.24%',
+        fontSize: 77,
+        lineHeight: 92,
+    },
+    heading3: {
+        left: '45.0%',
+        right: '4.82%',
+        top: '-27%',
+        bottom: '67.9%',
+        fontSize: 88,
+        lineHeight: 106,
+    },
+    paragraph: {
+        fontFamily: 'Roboto',
+        top: '-90%',
+        width: '305px',
+        height: '160px',
+        fontSize: 20,
+        lineHeight: 30,
+        letterSpacing: -0.01,
+    }
+});
