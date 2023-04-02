873c6a7c7cb95c9d96f30b642220bb482c69ab5c
Florencia <fsardella@fi.uba.ar>
2023-04-02 13:07:40 -0300

    Changed inconsistent identation

--- Fiufit-mobile/app/screens/login.tsx
+++ Fiufit-mobile/app/screens/login.tsx
@@ -7,135 +7,136 @@ import { useState } from 'react';
 import { auth, logInWithEmailAndPassword } from '../../firebase';
 
 export default function LoginScreen({ navigation } : any) {
-    const theme = extendTheme({
-        components: {
-            Text: {
-                defaultProps: {
-                    color: '#AEAEAE',
-                },
-            },
-            Button: {
-                defaultProps: {
-                    background: "#FF6060",
-                }
-            },
-            Heading: {
-                defaultProps: {
-                    color: '#FF6060',
-                }
-            }
+  const theme = extendTheme({
+    components: {
+      Text: {
+        defaultProps: {
+            color: '#AEAEAE',
+        },
+      },
+      Button: {
+        defaultProps: {
+            background: "#FF6060",
         }
-    });
-
-    const [show, setShow] = useState(false);
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [user, loading, error] = useAuthState(auth);
-    React.useEffect(() => {
-        if (loading) {
-          // maybe trigger a loading screen
-          return;
+      },
+      Heading: {
+        defaultProps: {
+            color: '#FF6060',
         }
-        if (user) navigation.navigate('HomeScreen');
-    }, [user, loading]);
+      }
+    }
+  });
 
-    return <NativeBaseProvider theme={ theme }>
-        <Stack space={ 3 } w="100%" alignItems="center" style={ styles.stack }>
-            <Heading style={ styles.heading }>Ingresa tus datos</Heading>
-            <Input
-                w={{ base: "80%", md: "30%" }} 
-                h="15%" 
-                variant="underlined" 
-                placeholder="Email" 
-                onChangeText={ (email) => setEmail(email) }
-            />
-            <Input w={{ base: "80%", md: "25%" }}
-                h="15%"
-                type={ show ? "text" : "password" }
-                variant="underlined"
-                onChangeText={ (password) => setPassword(password) }
-                InputRightElement={<Pressable onPress={ () => setShow(!show) }>
-                <Icon as={<MaterialIcons name={ show ? "visibility" : "visibility-off" } />}
-                size={ 5 } mr="2" color="muted.400" />
-                </Pressable>} placeholder="Contraseña"
-            />
-        </Stack>
-        <Flex h="12" alignItems="center">
-            <Button
-                style={ styles.button }
-                onPress={ () => {logInWithEmailAndPassword(email, password)} }
-                _text={{ color: "#FFFFFF", fontSize: "20px", fontWeight: "bold" }}
-            >
-                Iniciar sesión
-            </Button>
-        </Flex>
-        <Text style={ styles.registerTextOption }>O iniciar sesión con</Text>
-        <TouchableOpacity onPress={ () => {} }>
-            <Image
-                style={ styles.googleImage }
-                source={ require('../../assets/images/logos_google-icon.png') }
-                alt='google'/>
-        </TouchableOpacity>
-        <Text style={ styles.moveToRegister }>No tienes una cuenta?</Text>
-        <Link
-            style={ styles.link }
-            onPress={ () => {navigation.navigate('RegisterScreen')} }
-            _text={{ color: "#BC2666" }}
-        >
-            Registrarse
-        </Link>
-    </NativeBaseProvider>;
+  const [show, setShow] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, loading, error] = useAuthState(auth);
+  React.useEffect(() => {
+    if (loading) {
+      // maybe trigger a loading screen
+      return;
+    }
+    if (user) navigation.navigate('HomeScreen');
+  }, [user, loading]);
+
+  return <NativeBaseProvider theme={ theme }>
+    <Stack space={ 3 } w="100%" alignItems="center" style={ styles.stack }>
+      <Heading style={ styles.heading }>Ingresa tus datos</Heading>
+      <Input
+        w={{ base: "80%", md: "30%" }} 
+        h="15%" 
+        variant="underlined" 
+        placeholder="Email" 
+        onChangeText={ (email) => setEmail(email) }
+      />
+      <Input w={{ base: "80%", md: "25%" }}
+        h="15%"
+        type={ show ? "text" : "password" }
+        variant="underlined"
+        onChangeText={ (password) => setPassword(password) }
+        InputRightElement={<Pressable onPress={ () => setShow(!show) }>
+        <Icon as={<MaterialIcons name={ show ? "visibility" : "visibility-off" } />}
+        size={ 5 } mr="2" color="muted.400" />
+        </Pressable>} placeholder="Contraseña"
+      />
+    </Stack>
+    <Flex h="12" alignItems="center">
+      <Button
+        style={ styles.button }
+        onPress={ () => {logInWithEmailAndPassword(email, password)} }
+        _text={{ color: "#FFFFFF", fontSize: "20px", fontWeight: "bold" }}
+      >
+        Iniciar sesión
+      </Button>
+    </Flex>
+    <Text style={ styles.registerTextOption }>O iniciar sesión con</Text>
+    <TouchableOpacity onPress={ () => {} }>
+      <Image
+        style={ styles.googleImage }
+        source={ require('../../assets/images/logos_google-icon.png') }
+        alt='google'
+      />
+    </TouchableOpacity>
+    <Text style={ styles.moveToRegister }>No tienes una cuenta?</Text>
+    <Link
+      style={ styles.link }
+      onPress={ () => {navigation.navigate('RegisterScreen')} }
+      _text={{ color: "#BC2666" }}
+    >
+      Registrarse
+    </Link>
+  </NativeBaseProvider>;
 }
 
 const styles = StyleSheet.create({
-    button: {
-        flex: 1,
-        width: "90%",
-        left: "2.3%",
-        top: "35%",
-        borderRadius: 30
-    },
-    heading: {
-        flex: 0,
-        left: '1%',
-        top: '-20%',
-        fontFamily: 'Roboto',
-        fontStyle: 'normal',
-        fontWeight: '800',
-        fontSize: 30,
-        lineHeight: 35,
-        textAlign: 'center',
-    },
-    googleImage: {
-        top: "1300%",
-        left: "47%",
-        right: "0%",
-        bottom: "0%",
-    },
-    link: {
-        top: "81.35%",
-        left: "27%",
-    },
-    stack: {
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        top: '20%',
-    },
-    registerTextOption: {
-        top: '26.5%',
-        fontFamily: 'Montserrat',
-        fontStyle: 'normal',
-        fontWeight: '500',
-        fontSize: 15,
-        lineHeight: 21,
-        display: 'flex',
-        alignItems: 'center',
-        textAlign: 'center',
-    },
-    moveToRegister: {
-        top: '40%',
-        left: '20%',
-    }
+  button: {
+    flex: 1,
+    width: "90%",
+    left: "2.3%",
+    top: "35%",
+    borderRadius: 30
+  },
+  heading: {
+    flex: 0,
+    left: '1%',
+    top: '-20%',
+    fontFamily: 'Roboto',
+    fontStyle: 'normal',
+    fontWeight: '800',
+    fontSize: 30,
+    lineHeight: 35,
+    textAlign: 'center',
+  },
+  googleImage: {
+    top: "1300%",
+    left: "47%",
+    right: "0%",
+    bottom: "0%",
+  },
+  link: {
+    top: "81.35%",
+    left: "27%",
+  },
+  stack: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    top: '20%',
+  },
+  registerTextOption: {
+    top: '26.5%',
+    fontFamily: 'Montserrat',
+    fontStyle: 'normal',
+    fontWeight: '500',
+    fontSize: 15,
+    lineHeight: 21,
+    display: 'flex',
+    alignItems: 'center',
+    textAlign: 'center',
+  },
+  moveToRegister: {
+    top: '40%',
+    left: '20%',
+  }
 });
--- Fiufit-mobile/app/screens/welcome.tsx
+++ Fiufit-mobile/app/screens/welcome.tsx
@@ -5,136 +5,136 @@ import { Center, Container, Box, Button, AspectRatio, Image, Text, NativeBasePro
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function WelcomeScreen({navigation} : NativeStackScreenProps<any, any>) {
-    const theme = extendTheme({
-        components: {
-            Button: {
-                defaultProps: {
-                    background: '#FF6060',
-                },
-            },
-            Text: {
-                variants: {
-                    heading1: {
-                        color: '#FF6060'
-                    },
-                    heading2: {
-                        color: '#616161'
-                    },
-                    heading3: {
-                        color: '#000000'
-                    },
-                    paragraph: {
-                        color: '#212121',
-                    }
-                }
-            }
+  const theme = extendTheme({
+    components: {
+      Button: {
+        defaultProps: {
+            background: '#FF6060',
+        },
+      },
+      Text: {
+        variants: {
+          heading1: {
+            color: '#FF6060'
+          },
+          heading2: {
+            color: '#616161'
+          },
+          heading3: {
+            color: '#000000'
+          },
+          paragraph: {
+            color: '#212121',
+          }
         }
-    });
+      }
+    }
+  });
 
-    return <SafeAreaProvider>
-        <NativeBaseProvider theme={theme}>
-            <VStack space={4} alignItems="center">
-                <Text style={[styles.text, styles.heading1]} variant="heading1"> DESAFIA </Text>
-                <HStack space={ 2 } justifyContent="center">
-                    <Text style={[styles.text, styles.heading2]} variant="heading2" > TUS </Text>
-                    <Text style={[styles.text, styles.heading3]} variant="heading3"> LIMITES </Text>
-                </HStack>
-                <Center>
-                    <Flex width="xs">
-                        <Text style={[styles.text, styles.paragraph]} variant="paragraph">
-                            Realizá un seguimiento de tus entrenamientos, obtén mejores resultados y sé la mejor versión de ti mismo.
-                        </Text>
-                    </Flex>
-                    <Center style={{top:"15%"}}>
-                        <AspectRatio
-                            w="100%" 
-                            ratio={16 / 9}
-                        >
-                            <Image
-                                source={require('../../assets/images/welcome_image.png')} 
-                                alt="image"
-                            />
-                        </AspectRatio>
-                    </Center>
-                </Center>
-                <Button
-                    style={[styles.button, styles.firstButton]}
-                    _text={{color: "#FFFFFF"}}
-                    variant="firstButton"
-                    onPress={() => navigation.navigate('LoginScreen')}
-                >
-                    Iniciar sesión
-                </Button>
-                <Button
-                    style={[styles.button, styles.secondButton]}
-                    _text={{color: "#FFFFFF"}}
-                    variant="secondButton"
-                    onPress={() => navigation.navigate('RegisterScreen')}
-                >
-                    Registrarse
-                </Button>
-            </VStack>
-        </NativeBaseProvider>
-    </SafeAreaProvider>;
+  return <SafeAreaProvider>
+    <NativeBaseProvider theme={theme}>
+      <VStack space={4} alignItems="center">
+        <Text style={[styles.text, styles.heading1]} variant="heading1"> DESAFIA </Text>
+        <HStack space={ 2 } justifyContent="center">
+          <Text style={[styles.text, styles.heading2]} variant="heading2" > TUS </Text>
+          <Text style={[styles.text, styles.heading3]} variant="heading3"> LIMITES </Text>
+        </HStack>
+        <Center>
+          <Flex width="xs">
+            <Text style={[styles.text, styles.paragraph]} variant="paragraph">
+              Realizá un seguimiento de tus entrenamientos, obtén mejores resultados y sé la mejor versión de ti mismo.
+            </Text>
+          </Flex>
+            <Center style={{top:"15%"}}>
+              <AspectRatio
+                w="100%" 
+                ratio={16 / 9}
+              >
+                <Image
+                  source={require('../../assets/images/welcome_image.png')} 
+                  alt="image"
+                />
+              </AspectRatio>
+            </Center>
+        </Center>
+        <Button
+          style={[styles.button, styles.firstButton]}
+          _text={{color: "#FFFFFF"}}
+          variant="firstButton"
+          onPress={() => navigation.navigate('LoginScreen')}
+        >
+          Iniciar sesión
+        </Button>
+        <Button
+          style={[styles.button, styles.secondButton]}
+          _text={{color: "#FFFFFF"}}
+          variant="secondButton"
+          onPress={() => navigation.navigate('RegisterScreen')}
+        >
+          Registrarse
+        </Button>
+      </VStack>
+    </NativeBaseProvider>
+  </SafeAreaProvider>;
 }
 
 const styles = StyleSheet.create({
-    button: {
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        alignSelf: 'center',
-        paddingLeft: '10%',
-        paddingRight: '10%',
-        gap: 10,
-        position: 'absolute',
-        width: '35%',
-        height: '8%',
-        top: '118%',
-        borderRadius: 50,
-    },
-    firstButton: {
-        left: '10%',
-    },
-    secondButton: {
-        left: '55%',
-    },
-    textBox: {
-        textAlign: 'center',
-        flexDirection: 'row',
-    },
-    text: {
-        fontFamily: 'BebasNeue',
-        fontStyle: 'normal',
-        fontWeight: '400',
-    },
-    heading1: {
-        left: '-10%',
-        top: '20%',
-        fontSize: 100,
-        lineHeight: 120,
-    },
-    heading2: {
-        left: '30%',
-        top: '25%',
-        bottom: '68.24%',
-        fontSize: 77,
-        lineHeight: 92,
-    },
-    heading3: {
-        top: '23%',
-        left: '5%',
-        fontSize: 88,
-        lineHeight: 106,
-    },
-    paragraph: {
-        textAlign: 'center',
-        alignSelf: 'center',
-        fontFamily: 'Roboto',
-        top: '70%',
-        fontSize: 20,
-        lineHeight: 30,
-        letterSpacing: -0.01,
-    }
+  button: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+    paddingLeft: '10%',
+    paddingRight: '10%',
+    gap: 10,
+    position: 'absolute',
+    width: '35%',
+    height: '8%',
+    top: '118%',
+    borderRadius: 50,
+  },
+  firstButton: {
+    left: '10%',
+  },
+  secondButton: {
+    left: '55%',
+  },
+  textBox: {
+    textAlign: 'center',
+    flexDirection: 'row',
+  },
+  text: {
+    fontFamily: 'BebasNeue',
+    fontStyle: 'normal',
+    fontWeight: '400',
+  },
+  heading1: {
+    left: '-10%',
+    top: '20%',
+    fontSize: 100,
+    lineHeight: 120,
+  },
+  heading2: {
+    left: '30%',
+    top: '25%',
+    bottom: '68.24%',
+    fontSize: 77,
+    lineHeight: 92,
+  },
+  heading3: {
+    top: '23%',
+    left: '5%',
+    fontSize: 88,
+    lineHeight: 106,
+  },
+  paragraph: {
+    textAlign: 'center',
+    alignSelf: 'center',
+    fontFamily: 'Roboto',
+    top: '70%',
+    fontSize: 20,
+    lineHeight: 30,
+    letterSpacing: -0.01,
+  }
 });
