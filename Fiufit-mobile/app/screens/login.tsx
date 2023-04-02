7541dc46a5f0c610f13d9c1cafbab3f6eb49bd2f
Florencia <fsardella@fi.uba.ar>
2023-04-02 00:59:58 -0300

    Changed styles from login and register pages

--- Fiufit-mobile/app/screens/login.tsx
+++ Fiufit-mobile/app/screens/login.tsx
@@ -1,79 +1,24 @@
 import * as React from 'react';
-import { TouchableOpacity } from 'react-native';
-import { Container, Heading, Image, Link, Input, Button, Text, Icon, Stack, Pressable, NativeBaseProvider, extendTheme } from "native-base";
+import { TouchableOpacity, StyleSheet } from 'react-native';
+import { Container, Heading, Flex, Image, Link, Input, Button, Text, Icon, Stack, Pressable, NativeBaseProvider, extendTheme } from "native-base";
 import { MaterialIcons } from "@expo/vector-icons";
 
 
 export default function LoginScreen({ navigation } : any) {
     const theme = extendTheme({
         components: {
-            Image: {
-                defaultProps: {
-                    top: "1250%",
-                    left: "47%",
-                    right: "0%",
-                    bottom: "0%",
-                }
-            },
-            Link: {
-                defaultProps: {
-                    top: "81.3%",
-                    left: "27%",
-                }
-            },
             Text: {
-                variants: {
-                    ingresoAlternativo: {
-                        top: '25%',
-                        fontFamily: 'Montserrat',
-                        fontStyle: 'normal',
-                        fontWeight: '500',
-                        fontSize: '15px',
-                        lineHeight: '21px',
-                        display: 'flex',
-                        alignItems: 'center',
-                        textAlign: 'center',
-                        color: '#AEAEAE'
-                    },
-                    registrarse: {
-                        top: '40%',
-                        left: '20%',
-                        color: '#AEAEAE'
-                    }
-                }
+                defaultProps: {
+                    color: '#AEAEAE',
+                },
             },
             Button: {
                 defaultProps: {
-                    width: "350px",
-                    height: "55px",
-                    left: "22px",
-                    top: "40%",
                     background: "#FF6060",
-                    borderRadius: "30px",
-                }
-            },
-            Stack: {
-                variants: {
-                    inputLogin: {
-                        display: 'flex',
-                        flexDirection: 'column',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        top: '15%',
-                    }
                 }
             },
             Heading: {
                 defaultProps: {
-                    height: '38px',
-                    left: '25%',
-                    top: '350%',
-                    fontFamily: 'Roboto',
-                    fontStyle: 'normal',
-                    fontWeight: '800',
-                    fontSize: '30px',
-                    lineHeight: '35px',
-                    textAlign: 'center',
                     color: '#FF6060',
                 }
             }
@@ -84,9 +29,9 @@ export default function LoginScreen({ navigation } : any) {
 
     return <NativeBaseProvider theme={theme}>
         <Container>
-            <Heading>Ingresa tus datos</Heading>
+            <Heading style={styles.heading}>Ingresa tus datos</Heading>
         </Container>
-        <Stack space={4} w="100%" alignItems="center" variant="inputLogin">
+        <Stack space={4} w="100%" alignItems="center" style={styles.stack}>
             <Input w={{ base: "80%", md: "30%"}} h="15%" variant="underlined" placeholder="Email" />
             <Input w={{base: "80%", md: "25%"}}
             h="15%"
@@ -97,16 +42,80 @@ export default function LoginScreen({ navigation } : any) {
             size={5} mr="2" color="muted.400" />
             </Pressable>} placeholder="Contraseña" />
         </Stack>
-        <Button
-        onPress={() => {navigation.navigate('HomeScreen')}}
-        _text={{color: "#FFFFFF", fontSize: "20px", fontWeight: "bold"}}>Iniciar sesión</Button>
-        <Text variant="ingresoAlternativo">O iniciar sesión con</Text>
+        <Flex h="12" alignItems="center">
+            <Button
+            style={styles.button}
+            onPress={() => {navigation.navigate('HomeScreen')}}
+            _text={{color: "#FFFFFF", fontSize: "20px", fontWeight: "bold"}}>
+                Iniciar sesión
+            </Button>
+        </Flex>
+        <Text style={styles.registerTextOption}>O iniciar sesión con</Text>
         <TouchableOpacity onPress={() => {}}>
-            <Image source={require('../../assets/images/logos_google-icon.png')} alt='google'/>
+            <Image
+            style={styles.googleImage}
+            source={require('../../assets/images/logos_google-icon.png')}
+            alt='google'/>
         </TouchableOpacity>
-        <Text variant="registrarse">No tienes una cuenta?</Text>
-        <Link onPress={() => {navigation.navigate('RegisterScreen')}} _text={{color: "#BC2666"}}>
+        <Text style={styles.moveToRegister}>No tienes una cuenta?</Text>
+        <Link
+        style={styles.link}
+        onPress={() => {navigation.navigate('RegisterScreen')}}
+        _text={{color: "#BC2666"}}>
             Registrarse
         </Link>
     </NativeBaseProvider>;
 }
+
+const styles = StyleSheet.create({
+    button: {
+        flex: 1,
+        width: "90%",
+        left: "2.3%",
+        top: "35%",
+        borderRadius: 30
+    },
+    heading: {
+        flex: 0,
+        left: '25%',
+        top: '450%',
+        fontFamily: 'Roboto',
+        fontStyle: 'normal',
+        fontWeight: '800',
+        fontSize: 30,
+        lineHeight: 35,
+        textAlign: 'center',
+    },
+    googleImage: {
+        top: "1300%",
+        left: "47%",
+        right: "0%",
+        bottom: "0%",
+    },
+    link: {
+        top: "81.35%",
+        left: "27%",
+    },
+    stack: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        top: '20%',
+    },
+    registerTextOption: {
+        top: '26.5%',
+        fontFamily: 'Montserrat',
+        fontStyle: 'normal',
+        fontWeight: '500',
+        fontSize: 15,
+        lineHeight: 21,
+        display: 'flex',
+        alignItems: 'center',
+        textAlign: 'center',
+    },
+    moveToRegister: {
+        top: '40%',
+        left: '20%',
+    }
+});
--- Fiufit-mobile/app/screens/register.tsx
+++ Fiufit-mobile/app/screens/register.tsx
@@ -1,82 +1,27 @@
 import * as React from 'react';
-import { TouchableOpacity } from 'react-native';
-import { Container, Heading, Stack, Input, Image, Icon, Button, Link, Text, NativeBaseProvider, extendTheme, Pressable } from 'native-base';
+import { TouchableOpacity, StyleSheet } from 'react-native';
+import { Container, Flex, Heading, Stack, Input, Image, Icon, Button, Link, Text, NativeBaseProvider, extendTheme, Pressable } from 'native-base';
 import { MaterialIcons } from "@expo/vector-icons";
 
 
 export default function RegisterScreen({ navigation } : any) {
   const theme = extendTheme({
     components: {
-      Image: {
-        defaultProps: {
-            top: "780%",
-            left: "47%",
-            right: "0%",
-            bottom: "0%",
-        }
-      },
       Text: {
-        variants: {
-            ingresoAlternativo: {
-                top: '15%',
-                fontFamily: 'Montserrat',
-                fontStyle: 'normal',
-                fontWeight: '500',
-                fontSize: '15px',
-                lineHeight: '21px',
-                display: 'flex',
-                alignItems: 'center',
-                textAlign: 'center',
-                color: '#AEAEAE'
-            },
-            iniciarSesion: {
-                top: '25%',
-                left: '20%',
-                color: '#AEAEAE'
-            }
+        defaultProps: {
+          color: '#AEAEAE',
         }
       },
       Heading: {
           defaultProps: {
-            height: '38px',
-            left: '25%',
-            top: '350%',
-            fontFamily: 'Roboto',
-            fontStyle: 'normal',
-            fontWeight: '800',
-            fontSize: '30px',
-            lineHeight: '35px',
-            textAlign: 'center',
             color: '#FF6060',
           }
       },
-      Stack: {
-        variants: {
-            inputLogin: {
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'center',
-                top: '15%',
-            }
-        }
-      },
       Button: {
         defaultProps: {
-            width: "350px",
-            height: "55px",
-            left: "22px",
-            top: "20%",
             background: "#FF6060",
-            borderRadius: "30px",
         }
-      },
-      Link: {
-        defaultProps: {
-            top: "48.9%",
-            left: "27%",
-        }
-      },
+      }
     }
   });
 
@@ -84,9 +29,13 @@ export default function RegisterScreen({ navigation } : any) {
 
   return <NativeBaseProvider theme={theme}>
     <Container>
-      <Heading>Crear una cuenta</Heading>
+      <Heading style={styles.heading} >Crear una cuenta</Heading>
     </Container>
-    <Stack space={4} w="100%" alignItems="center" variant="inputLogin">
+    <Stack
+    space={4} w="100%"
+    alignItems="center"
+    style={styles.stack}
+    >
       <Input w={{ base: "80%", md: "30%"}} h="15%" variant="underlined" placeholder="Nombre completo" />
       <Input w={{ base: "80%", md: "30%"}} h="15%" variant="underlined" placeholder="Email" />
       <Input w={{base: "80%", md: "25%"}}
@@ -98,16 +47,78 @@ export default function RegisterScreen({ navigation } : any) {
       size={5} mr="2" color="muted.400" />
       </Pressable>} placeholder="Contraseña" />
     </Stack>
-    <Button
-        onPress={() => {navigation.navigate('HomeScreen')}}
-        _text={{color: "#FFFFFF", fontSize: "20px", fontWeight: "bold"}}>Registrarse</Button>
-        <Text variant="ingresoAlternativo">O registrarse con</Text>
-        <TouchableOpacity onPress={() => {}}>
-            <Image source={require('../../assets/images/logos_google-icon.png')} alt='google'/>
-        </TouchableOpacity>
-        <Text variant="iniciarSesion">No tienes una cuenta?</Text>
-        <Link onPress={() => {navigation.navigate('LoginScreen')}} _text={{color: "#BC2666"}}>
-            Iniciar sesión
-        </Link>
+    <Flex h="12" alignItems="center">
+      <Button
+      style={styles.button}
+      onPress={() => {navigation.navigate('HomeScreen')}}
+      _text={{color: "#FFFFFF", fontSize: "20px", fontWeight: "bold"}}>Registrarse</Button>
+    </Flex>
+    <Text style={styles.loginTextOption} >O registrarse con</Text>
+    <TouchableOpacity onPress={() => {}}>
+        <Image
+        style={styles.googleImage}
+        source={require('../../assets/images/logos_google-icon.png')}
+        alt='google'/>
+    </TouchableOpacity>
+    <Text style={styles.moveToLogin} >No tienes una cuenta?</Text>
+    <Link
+    style={styles.link}
+    onPress={() => {navigation.navigate('LoginScreen')}} 
+    _text={{color: "#BC2666"}}>
+        Iniciar sesión
+    </Link>
   </NativeBaseProvider>;
 }
+
+const styles = StyleSheet.create({
+  button: {
+    flex: 1,
+    width: "90%",
+    left: "2.3%",
+    top: "20%",
+    borderRadius: 30
+  },
+  heading: {
+    flex: 0,
+    left: '27%',
+    top: '450%',
+    fontFamily: 'Roboto',
+    fontStyle: 'normal',
+    fontWeight: '800',
+    fontSize: 30,
+    lineHeight: 35,
+    textAlign: 'center',
+  },
+  googleImage: {
+    top: "780%",
+    left: "47%",
+    right: "0%",
+    bottom: "0%",
+  },
+  link: {
+    top: "48.9%",
+    left: "27%",
+  },
+  stack: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    top: '15%',
+  },
+  loginTextOption: {
+    top: '15%',
+    fontFamily: 'Montserrat',
+    fontStyle: 'normal',
+    fontWeight: '500',
+    fontSize: 15,
+    lineHeight: 21,
+    display: 'flex',
+    alignItems: 'center',
+    textAlign: 'center',
+  },
+  moveToLogin: {
+    top: '25%',
+    left: '20%',
+  }
+});
