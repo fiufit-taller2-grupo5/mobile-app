ca14acb07b313f4482000adde43206d0ac550e43
Gabrielsem <gsemorile@fi.uba.ar>
2023-05-11 15:09:18 -0300

    Fix cycle dependency warning

--- Fiufit-mobile/api.ts
+++ Fiufit-mobile/api.ts
@@ -1,6 +1,5 @@
 import { User } from "firebase/auth";
 import { getUserFromStorage, storeUserOnStorage, userInfo } from "./app/utils/storageController";
-import { auth } from "./firebase";
 
 type userDetails = {
     userId?: number;
@@ -44,7 +43,7 @@ export const createUser = async (user: User, emailRegisterName : string = "defau
         try {
           const responseData = await response.json();
           const internal_id = getInternalIdFromResponse(responseData);
-          const userInfo = await getUserInfoById(internal_id);
+          const userInfo = await getUserInfoById(internal_id, user);
 
           if (userInfo instanceof Error) {
             throw userInfo;
@@ -201,10 +200,10 @@ export async function getTrainings(url:string) : Promise<string[] | null> {
   return null;
 }
 
-export async function getUserInfoByEmail(email:string) : Promise<userInfo | Error> {
+export async function getUserInfoByEmail(email:string, user: User) : Promise<userInfo | Error> {
+  console.log("getting user info by email of user ", user );
   const url = "https://api-gateway-prod-szwtomas.cloud.okteto.net/user-service/api/users/?email=" + email;
-  console.log("getting user info by email: ", url);
-  const accessToken = (auth.currentUser as any).stsTokenManager.accessToken;
+  const accessToken = (user as any).stsTokenManager.accessToken;
   try {
     const response = await fetch(url, {
       method: "GET",
@@ -216,11 +215,9 @@ export async function getUserInfoByEmail(email:string) : Promise<userInfo | Erro
         "Authorization": "Bearer " + accessToken,
       },
     });
-    // console.log("getUserInfoByEmail->RESPONSE:", response);
     if (response.ok) {
       try {
         const user : userInfo = await response.json();
-        console.log("user info:", user);
         return user;
       } catch (err: any) {
         console.error(err);
@@ -235,10 +232,11 @@ export async function getUserInfoByEmail(email:string) : Promise<userInfo | Erro
 }
 
 // TODO refactor methods to reduce code redundancy
-export async function getUserInfoById(id:string) : Promise<userInfo | Error> {
+export async function getUserInfoById(id:string, user: User) : Promise<userInfo | Error> {
+  console.log("getting user info by id of user ", user );
   const url = "https://api-gateway-prod-szwtomas.cloud.okteto.net/user-service/api/users/?id=" + id;
   console.log("getting user info by id: ", url);
-  const accessToken = (auth.currentUser as any).stsTokenManager.accessToken;
+  const accessToken = (user as any).stsTokenManager.accessToken;
   try {
     const response = await fetch(url, {
       method: "GET",
@@ -253,7 +251,6 @@ export async function getUserInfoById(id:string) : Promise<userInfo | Error> {
     if (response.ok) {
       try {
         const user : userInfo = await response.json();
-        console.log("user info:", user);
         return user;
       } catch (err: any) {
         console.error(err);
