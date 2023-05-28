f29c7d0595eeebd8c707edaec2fbafd6dd99f207
mpiligaddi <mgaddi@fi.uba.ar>
2023-05-28 17:56:41 -0300

    Added filters on trainingList

--- Fiufit-mobile/app/components/trainings/searchBar.tsx
+++ Fiufit-mobile/app/components/trainings/searchBar.tsx
@@ -1,14 +1,45 @@
-import { Box, Divider, Heading, Icon, Input, VStack } from "native-base";
+import { Icon, Input, VStack } from "native-base";
 import React from "react";
 import { MaterialIcons } from "@expo/vector-icons";
-import { Training } from "../../../api";
 
-export default function SearchBar() {
-    return <VStack mx="1" my="3" space={2} w="100%" maxW="380px" backgroundColor="#fff" divider={<Box px="2">
-        <Divider />
-        </Box>}>
-        <VStack w="100%" space={5} alignSelf="center">
-            <Input placeholder="Search trainings" width="100%" borderRadius="4" py="3" px="1" fontSize="14" InputLeftElement={<Icon m="2" ml="3" size="6" color="gray.400" as={<MaterialIcons name="search" />} />} />
-        </VStack>
-    </VStack>;
-}
+export const SearchBar = ({text, searchText, trainings, filterData}: any) => {
+    
+    const filterDataByTitle = (text: string) => {
+        if (text) {
+          const filtered = trainings.filter(
+            (item: { title: string; }) =>
+              item.title.toLowerCase().includes(text.toLowerCase())
+          );
+          filterData(filtered);
+        } else {
+          filterData(trainings);
+        }
+    };
+    
+    const handleSearch = (text: string) => {
+        searchText(text);
+        filterDataByTitle(text);
+      };
+
+    return <VStack alignSelf="center">
+        <Input
+        placeholder="Search trainings by name"
+        onChangeText={handleSearch}
+        value={text}
+        width="100%"
+        borderRadius="4"
+        fontSize="14"
+        InputLeftElement={
+            <Icon
+            m="2"
+            ml="3"
+            size="6"
+            color="gray.400"
+            as={<MaterialIcons name="search" />}
+            />
+        }
+        />
+    </VStack>
+  };
+  
+  
--- Fiufit-mobile/app/components/trainings/trainingsList.tsx
+++ Fiufit-mobile/app/components/trainings/trainingsList.tsx
@@ -1,28 +1,19 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
-  Image,
   Box,
   FlatList,
   HStack,
   VStack,
-  Text,
-  NativeBaseProvider,
-  Button,
   Divider,
   Icon,
   Input,
   View,
   CheckIcon,
   Select,
-  Center
 } from "native-base";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import { trainingStyles } from "../../styles";
 import {
-  addFavoriteTraining,
   getFavoriteTrainings,
   getTrainings,
-  quitFavoriteTraining,
   Training,
 } from "../../../api";
 import { MaterialIcons } from "@expo/vector-icons";
@@ -33,165 +24,43 @@ interface Props {
   navigation: any;
 }
 
-interface TrainingInfoProps {
-  navigation: any;
-  training: Training;
-}
-
-const mainImage = (training_type: any) => {
-  if (training_type === "Running")
-    return "https://wallpaperaccess.com/thumb/2604922.jpg";
-  else if (training_type === "Swimming")
-    return "https://wallpaperaccess.com/thumb/1634055.jpg";
-  else if (training_type === "Walking")
-    return "https://wallpaperaccess.com/thumb/654835.jpg";
-  else if (training_type === "Biking")
-    return "https://wallpaperaccess.com/thumb/4431599.jpg";
-  else if (training_type === "Yoga")
-    return "https://wallpaperaccess.com/thumb/2532294.jpg";
-  else if (training_type === "Basketball")
-    return "https://wallpaperaccess.com/thumb/798750.jpg";
-  else if (training_type === "Football")
-    return "https://wallpaperaccess.com/thumb/1813065.jpg";
-  else if (training_type === "Gymnastics")
-    return "https://wallpaperaccess.com/thumb/2236559.jpg";
-  else if (training_type === "Dancing")
-    return "https://wallpaperaccess.com/thumb/1315981.jpg";
-  else if (training_type === "Hiking")
-    return "https://wallpaperaccess.com/thumb/7309738.jpg";
-};
-
-const TrainingsInfo = (props: TrainingInfoProps) => {
-  const { navigation, training } = props;
-  const [isFavorite, setTrainingFavorite] = useState<Boolean>(
-    training.isFavorite || false
-  );
-  const [isRated, setTrainingRated] = useState<Boolean>(
-    true || false // training.isRated || false
-  );
-
-  return (
-    <Box backgroundColor="#fff">
-      <Button
-        height={150}
-        px="10"
-        py="10"
-        backgroundColor="#fff"
-        onPress={async () => {
-          navigation.navigate("TrainingInfoScreen", { trainingData: training });
-        }}
-      >
-        <HStack
-          space={[2, 3]}
-          justifyContent="space-between"
-          height={70}
-          width={380}
-        >
-          <Image
-            source={{ uri: mainImage(training.type) }}
-            alt="Alternate Text"
-            size="lg"
-            borderRadius={10}
-          />
-          <VStack my={1} width={220} height={10} mr={0} ml={1}>
-            <Text
-              style={trainingStyles.textTitle}
-              color="#000000"
-              text-align="left"
-              bold
-            >
-              {training.title}
-            </Text>
-            <Text fontSize="sm" color="#000000">
-              {training.description}
-            </Text>
-            <Text fontSize="xs" color="#000000">
-              Dificultad: {training.difficulty}
-            </Text>
-          </VStack>
-          <VStack my={1} width={30} height={10} mr={0} ml={1}>
-            <Button
-              backgroundColor="#fff"
-              onPress={async () => {
-                if (isFavorite) {
-                  setTrainingFavorite(false);
-                  const response = await quitFavoriteTraining(training.id);
-                  if (!response) setTrainingFavorite(true);
-                } else {
-                  setTrainingFavorite(true);
-                  const response = await addFavoriteTraining(training.id);
-                  if (!response) setTrainingFavorite(false);
-                }
-              }}
-            >
-              <Icon
-                as={
-                  <MaterialCommunityIcons
-                    name={isFavorite ? "heart" : "heart-outline"}
-                  />
-                }
-                size={6}
-                color="#FF6060"
-                alignSelf="center"
-              />
-            </Button>
-            <Button
-              backgroundColor="#fff"
-              onPress={async () => {
-                console.log("click");
-              }}
-            >
-              <Icon
-                as={
-                  <MaterialCommunityIcons
-                    name={isRated ? "star" : "star-outline"}
-                  />
-                }
-                size={6}
-                color="#FFD27D"
-                alignSelf="center"
-              />
-            </Button>
-          </VStack>
-        </HStack>
-        <Divider my={10} mx={1} />
-      </Button>
-    </Box>
-  );
-};
-
 export default function TrainingsList(props: Props) {
   const { navigation } = props;
   const [trainingsList, setTrainingsList] = useState<Training[]>([]);
-  const [searchText, setSearchText] = useState("");
+  const [selectedTitle, setTitle] = useState("");
   const [filteredData, setFilteredData] = useState<Training[]>([]);
   const [refreshing, setRefreshing] = useState(true);
-  const [type, setType] = React.useState("");
+  const [selectedType, setType] = React.useState("");
+  const [selectedDifficulty, setDifficulty] = React.useState("");
 
-  /*const filterDataByDifficultyOrType = (text: string) => {
-    if (text) {
+  const filterData = () => {
+    //if(selectedDifficulty && selectedType) {
       const filtered = trainingsList.filter(
         (item) =>
-          item.difficulty === parseInt(text) ||
-          item.type.toLowerCase().includes(text.toLowerCase())
+          (selectedDifficulty === '' || item.difficulty === parseInt(selectedDifficulty)) &&
+          (selectedType === '' || item.type.toLowerCase().includes(selectedType.toLowerCase())) &&
+          (selectedTitle === '' || item.title.toLowerCase().includes(selectedTitle.toLowerCase()))
       );
       setFilteredData(filtered);
-    } else {
-      setFilteredData(trainingsList);
-    }
-  };*/
-
-  const filterDataByType = (text: string) => {
-    if (text) {
+   /* }
+    else if(selectedDifficulty) {
       const filtered = trainingsList.filter(
         (item) =>
-          item.type.toLowerCase().includes(text.toLowerCase())
+          (item.difficulty === parseInt(selectedDifficulty))
       );
       setFilteredData(filtered);
-    } else {
-      setFilteredData(trainingsList);
     }
-  };
+    else if(selectedType) {
+      const filtered = trainingsList.filter(
+        (item) =>
+          (item.type.toLowerCase().includes(selectedType.toLowerCase()))
+      );
+      setFilteredData(filtered);
+    }
+    else {
+      setFilteredData(trainingsList);
+    }*/
+  }
 
   const filterDataByTitle = (text: string) => {
     if (text) {
@@ -205,14 +74,16 @@ export default function TrainingsList(props: Props) {
     }
   };
 
-  const handleSearch = (text: string) => {
-    setSearchText(text);
-    filterDataByTitle(text);
+  const handleFilterByTitle = (text: string) => {
+    setTitle(text);
+  };
+
+  const handleFilterByDifficulty = (text: string) => {
+    setDifficulty(text);
   };
 
   const handleFilterByType = (text: string) => {
     setType(text);
-    filterDataByType(text);
   };
 
   function updateFavoriteStatus(trainingResponse: Training[], favoriteTrainingResponse: Training[]): Training[] {
@@ -228,26 +99,14 @@ export default function TrainingsList(props: Props) {
     const trainingsResponse = await getTrainings();
     const favoritesTrainingsResponse = await getFavoriteTrainings();
     let trainings = updateFavoriteStatus(trainingsResponse, favoritesTrainingsResponse);
-    // const ratings = await getTrainingsRatings(trainings.map((training) => training.id));
-    // trainings = updateMeanRating(trainings, ratings);
     setTrainingsList(trainings);
     setRefreshing(false);
-    setFilteredData(trainings);
+    filterData();
   }
-
-  // function updateMeanRating(trainingResponse: Training[]): Training[] {
-  //   return trainingResponse.map((training) => ({
-  //     ...training,
-  //     meanRating: training.ratings.reduce(
-  //       (acc, rating) => acc + rating.value,
-  //       0
-  //     ),
-  //   }));
-  // }
 
   useEffect(() => {
     getTrainingsList();
-  }, [])
+  }, [selectedTitle, selectedType, selectedDifficulty])
 
   return (<View flex={1} backgroundColor="#fff">
     <VStack
@@ -263,9 +122,9 @@ export default function TrainingsList(props: Props) {
     >
       <VStack alignSelf="center">
         <Input
-          placeholder="Search trainings by name"
-          onChangeText={handleSearch}
-          value={searchText}
+          placeholder="Search trainings by title"
+          onChangeText={handleFilterByTitle}
+          value={selectedTitle}
           width="100%"
           borderRadius="4"
           fontSize="14"
@@ -280,13 +139,13 @@ export default function TrainingsList(props: Props) {
           }
         />
       </VStack>
-      <VStack>
+      <HStack>
       <Box maxW="300">
-        <Select selectedValue={type} maxWidth="190" accessibilityLabel="Choose Type" placeholder="Choose Type" _selectedItem={{
+        <Select selectedValue={selectedType} minWidth="180" maxWidth="190" accessibilityLabel="Choose Type" placeholder="Choose Type" _selectedItem={{
         bg: "teal.600",
         endIcon: <CheckIcon size="5" />
       }} mt={1} onValueChange={handleFilterByType}>
-          <Select.Item label="No filter" value="" />
+          <Select.Item label="All types" value="" />
           <Select.Item label="Running" value="Running" />
           <Select.Item label="Swimming" value="Swimming" />
           <Select.Item label="Biking" value="Biking" />
@@ -299,7 +158,25 @@ export default function TrainingsList(props: Props) {
           <Select.Item label="Hiking" value="Hiking" />
         </Select>
       </Box>
-    </VStack>
+      <Box maxW="300">
+        <Select selectedValue={selectedDifficulty} minWidth="180" maxWidth="190" accessibilityLabel="Choose Difficulty" placeholder="Choose Difficulty" _selectedItem={{
+        bg: "teal.600",
+        endIcon: <CheckIcon size="5" />
+      }} mt={1} onValueChange={handleFilterByDifficulty}>
+          <Select.Item label="All difficulties" value="" />
+          <Select.Item label="1" value="1" />
+          <Select.Item label="2" value="2" />
+          <Select.Item label="3" value="3" />
+          <Select.Item label="4" value="4" />
+          <Select.Item label="5" value="5" />
+          <Select.Item label="6" value="6" />
+          <Select.Item label="7" value="7" />
+          <Select.Item label="8" value="8" />
+          <Select.Item label="9" value="9" />
+          <Select.Item label="10" value="10" />
+        </Select>
+      </Box>
+    </HStack>
     </VStack>
     <View flex={1}>
       <FlatList
